fbb36f06c0d3aa0d09abebef2752df8d10e6ce07
hirrao <git@hirrao.com>
2025-02-18 11:40:14 +0800

    add recipes, refactor

--- src/main/java/com/hirrao/NHCraft/recipes/MachineRecipes/MEInputRecipes.java
+++ src/main/java/com/hirrao/NHCraft/recipes/MachineRecipes/MEInputRecipes.java
@@ -1,27 +1,27 @@
 package com.hirrao.NHCraft.recipes.MachineRecipes;
 
 import static com.hirrao.NHCraft.config.CommonConfig.enableMEInputRecipes;
+import static gregtech.api.enums.Mods.AE2FluidCraft;
+import static gregtech.api.enums.Mods.AppliedEnergistics2;
 import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 
 import net.minecraft.item.ItemStack;
 
 import com.hirrao.NHCraft.recipes.IRecipePoll;
 
-import cpw.mods.fml.common.registry.GameRegistry;
 import gregtech.api.enums.GTValues;
 import gregtech.api.enums.ItemList;
 import gregtech.api.recipe.RecipeMaps;
+import gregtech.api.util.GTModHandler;
 import gregtech.api.util.GTUtility;
 
 public class MEInputRecipes implements IRecipePoll {
 
-    public static final ItemStack FLUID_INTERFACE = new ItemStack(GameRegistry.findItem("ae2fc", "fluid_interface"));
-    public static final ItemStack PART_FLUID_INTERFACE = new ItemStack(
-        GameRegistry.findItem("ae2fc", "part_fluid_interface"));
-    public static final ItemStack ACCELERATION_CARD_4 = new ItemStack(
-        GameRegistry.findItem("appliedenergistics2", "item.ItemMultiMaterial"),
-        4,
-        30);
+    public static final ItemStack FLUID_INTERFACE = GTModHandler.getModItem(AE2FluidCraft.ID, "fluid_interface", 1);
+    public static final ItemStack PART_FLUID_INTERFACE = GTModHandler
+        .getModItem(AE2FluidCraft.ID, "part_fluid_interface", 1);
+    public static final ItemStack ACCELERATION_CARD_4 = GTModHandler
+        .getModItem(AppliedEnergistics2.ID, "item.ItemMultiMaterial", 4, 30);
 
     @Override
     public void loadRecipes() {
--- src/main/java/com/hirrao/NHCraft/recipes/MachineRecipes/ProgrammableHatchesRecipes.java
+++ src/main/java/com/hirrao/NHCraft/recipes/MachineRecipes/ProgrammableHatchesRecipes.java
@@ -2,17 +2,37 @@ package com.hirrao.NHCraft.recipes.MachineRecipes;
 
 import static com.hirrao.NHCraft.config.CommonConfig.enableMEInputRecipes;
 import static com.hirrao.NHCraft.config.CommonConfig.enableProgrammableHatchesRecipes;
+import static com.hirrao.NHCraft.utils.Mods.ProgrammableHatches;
+import static gregtech.api.enums.Mods.GregTech;
+import static gregtech.api.util.GTRecipeBuilder.SECONDS;
+
+import net.minecraft.item.ItemStack;
 
 import com.hirrao.NHCraft.recipes.IRecipePoll;
 
-import cpw.mods.fml.common.Loader;
+import gregtech.api.enums.GTValues;
+import gregtech.api.enums.ItemList;
+import gregtech.api.enums.Materials;
+import gregtech.api.recipe.RecipeMaps;
+import gregtech.api.util.GTModHandler;
 
 public class ProgrammableHatchesRecipes implements IRecipePoll {
 
     @Override
     public void loadRecipes() {
-        if (Loader.isModLoaded("programablehatches") && enableProgrammableHatchesRecipes && enableMEInputRecipes) {
+        if (ProgrammableHatches.isModLoaded() && enableProgrammableHatchesRecipes && enableMEInputRecipes) {
             // TODO
+            final ItemStack PROGRAMMING_COVER = GTModHandler
+                .getModItem(ProgrammableHatches.ID, "proghatches.cover", 1L, 0);
+            final ItemStack PROGRAMMABLE_CRAFTINGINPUT_BUFFER = GTModHandler
+                .getModItem(GregTech.ID, "gt.blockmachines", 1, 22609);
+            GTValues.RA.stdBuilder()
+                .itemInputs(ItemList.Hatch_CraftingInput_Bus_ME.get(1), PROGRAMMING_COVER)
+                .fluidInputs(Materials.AdvancedGlue.getFluid(4000))
+                .itemOutputs(PROGRAMMABLE_CRAFTINGINPUT_BUFFER)
+                .duration(20 * SECONDS)
+                .eut(7680)
+                .addTo(RecipeMaps.mixerRecipes);
         }
     }
 }
