cededefa1b098ffa241959cd654a7c9f5a9157ba
hirrao <git@hirrao.com>
2025-10-17 19:29:35 +0800

    添加编程样板输入镜像配方

--- src/main/java/com/hirrao/xnntech/recipes/MachineRecipes/MEInputRecipes.java
+++ src/main/java/com/hirrao/xnntech/recipes/MachineRecipes/MEInputRecipes.java
@@ -1,8 +1,7 @@
 package com.hirrao.xnntech.recipes.MachineRecipes;
 
 import static com.hirrao.xnntech.config.CommonConfig.enableMEInputRecipes;
-import static gregtech.api.enums.Mods.AE2FluidCraft;
-import static gregtech.api.enums.Mods.AppliedEnergistics2;
+import static gregtech.api.enums.Mods.*;
 import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 
 import net.minecraft.item.ItemStack;
@@ -20,6 +19,7 @@ public class MEInputRecipes {
         .getModItem(AE2FluidCraft.ID, "part_fluid_interface", 1);
     public static final ItemStack ACCELERATION_CARD_4 = GTModHandler
         .getModItem(AppliedEnergistics2.ID, "item.ItemMultiMaterial", 4, 30);
+    public static final ItemStack WIRELESS_KIT_2 = GTModHandler.getModItem(AE2Stuff.ID, "Wireless", 2);
 
     public static void loadRecipes() {
         if (enableMEInputRecipes) {
@@ -42,6 +42,16 @@ public class MEInputRecipes {
                 .itemOutputs(ItemList.Hatch_Input_ME.get(1))
                 .duration(15 * SECONDS)
                 .eut(480)
+                .addTo(RecipeMaps.assemblerRecipes);
+            GTValues.RA.stdBuilder()
+                .itemInputs(
+                    GTUtility.getIntegratedCircuit(3),
+                    WIRELESS_KIT_2,
+                    ItemList.Hatch_Input_ME.get(1),
+                    ItemList.Hatch_Input_Bus_ME.get(1))
+                .itemOutputs(ItemList.Hatch_CraftingInput_Bus_Slave.get(1))
+                .duration(15 * SECONDS)
+                .eut(7680)
                 .addTo(RecipeMaps.assemblerRecipes);
         }
 
